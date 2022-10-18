d8250154b7bdea61e6955700559bef0c7214669c
Sunilprasadk <78161108+Sunilprasadk@users.noreply.github.com>
2022-10-19 00:29:21 +0530

    Day-01 commit
    Contains hello world and comment program

---
+++ comment.c
@@ -0,0 +1,8 @@
+/*CODE BY : K.SUNILPRASAD
+DATE : 18-10-2022*/
+
+#include <stdio.h>//pre-processor file
+int main(){
+    printf("\nHello World");//built-in function from header file
+    return 0;
+}
